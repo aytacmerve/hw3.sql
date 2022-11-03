db0ccef906987687e77a7545d958ab57799cd1d2
aytacmerve <117162834+aytacmerve@users.noreply.github.com>
2022-11-03 21:45:42 +0300

    Update hw3.sql
    https://app.patika.dev/courses/sql/Odev3

--- hw3.sql
+++ hw3.sql
@@ -1,4 +1,4 @@
 SELECT * FROM country WHERE country LIKE 'A%a';
 SELECT * FROM country WHERE country LIKE '_____%n';
-SELECT title FROM film WHERE title LIKE 'T%' OR title LIKE '%t' OR title LIKE '%T' ;
+SELECT title FROM film WHERE title LIKE 'T%t%t%T%' OR title LIKE '%t%t%T%t%' OR title LIKE '%t%T%t%t%' OR title LIKE 'T%T%t%t%' OR title LIKE 'T%t%T%t%'OR title LIKE '%t%t%t%t%';
 SELECT * FROM film WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99 ;
